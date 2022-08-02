dd400c0a8710d5aaf5381c92715f75efb0c173e2
ddries <aescalona.public@gmail.com>
2022-08-02 13:10:11 +0200

    split into modules and added global context

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use std::{io, num::IntErrorKind};
+mod fsm;
+mod util;
 
-enum OwnState {
-    Idle,
-    ShowEntries,
-    AddEntry,
-    RemoveEntry,
-}
-
-enum MenuOption {
+pub enum MenuOption {
     ShowEntries = 0,
     AddEntry,
     RemoveEntry,
@@ -15,8 +9,8 @@ enum MenuOption {
 }
 
 #[derive(Debug)]
-struct TodoEntry {
-    description: String
+pub struct TodoEntry {
+    pub description: String
 }
 
 fn print_welcome() {
@@ -33,147 +27,25 @@ fn print_menu() {
     println!("4. Exit");
 }
 
-fn read_raw_input() -> Option<i32> {
-    let mut input = String::new();
-    let result = io::stdin().read_line(&mut input);
-
-    match result {
-        Ok(_) => {
-            let input = input.trim().parse::<i32>();
-            match input {
-                Ok(result) => Some(result),
-                Err(error) => {
-                    match error.kind() {
-                        IntErrorKind::Empty => (),
-                        _ => println!("could not parse stdin, {:?}", error)
-                    };
-                    None
-                },
-            }
-        },
-        Err(_) => {
-            None
-        },
-    }
-}
-
-fn int_to_menu_option(int: i32) -> MenuOption {
-    match int {
-        1 => MenuOption::ShowEntries,
-        2 => MenuOption::AddEntry,
-        3 => MenuOption::RemoveEntry,
-        _ => MenuOption::Exit,
-    }
-}
-
-fn state_logic(current_state: &mut OwnState, user_input: Option<i32>) {
-    match current_state {
-        &mut OwnState::Idle => {
-            if user_input.is_some() {
-                let user_input: i32 = user_input.unwrap();
-                let user_input = int_to_menu_option(user_input);
-
-                match user_input {
-                    MenuOption::ShowEntries => {
-                        *current_state = OwnState::ShowEntries;
-                    },
-                    MenuOption::AddEntry => {
-                        *current_state = OwnState::AddEntry;
-                    },
-                    MenuOption::RemoveEntry => {
-                        *current_state = OwnState::RemoveEntry;
-                    },
-                    MenuOption::Exit => {
-                        println!("good bye!");
-                        std::process::exit(0);
-                    }
-                };
-            }
-        },
-        _ => *current_state = OwnState::Idle,
-    }
-}
-
-fn output_logic(current_state: &OwnState, todo_entries: &mut Vec<TodoEntry>) {
-    match current_state {
-        OwnState::ShowEntries => {
-            let mut idx: u8 = 0;
-            for entry in todo_entries.iter() {
-                println!("entry {}: {}", idx, entry.description);
-                idx = idx + 1;
-            }
-        },
-
-        OwnState::AddEntry => {
-            println!("enter entry description");
-            let mut description = String::new();
-            let result = io::stdin().read_line(&mut description);
-            match result {
-                Ok(_) => {
-                    let new_entry: TodoEntry = TodoEntry { description };
-                    println!("added entry {:?}", new_entry);
-                    todo_entries.push(new_entry);
-                },
-                Err(_) => println!("could not read description"),
-            };
-        },
-
-        OwnState::RemoveEntry => {
-            println!("enter entry index");
-            let mut idx: u8 = 0;
-            for entry in todo_entries.iter() {
-                println!("entry {}: {}", idx, entry.description);
-                idx = idx + 1;
-            }
-            let mut idx = String::new();
-            let result = io::stdin().read_line(&mut idx);
-            match result {
-                Ok(_) => {
-                    let result = idx.trim().parse::<i32>();
-                    match result {
-                        Ok(result) => {
-                            if (result as usize) > todo_entries.len() || result < 0 {
-                                println!("given index is not valid!");
-                                return;
-                            }
-
-                            todo_entries.remove(result as usize);
-                            println!("removed index {}", result);
-                        },
-                        Err(error) => {
-                            match error.kind() {
-                                IntErrorKind::Empty => (),
-                                _ => println!("could not parse index, {:?}", error)
-                            };
-                        },
-                    };
-
-                },
-                Err(_) => println!("could not read index"),
-            };
-        },
-
-        _ => (),
-    };
-}
-
 fn main() {
-    let mut current_state: OwnState = OwnState::Idle;
-    let mut todo_entries: Vec<TodoEntry> = Vec::new();
+    let mut context = fsm::GlobalContext::new();
 
     print_welcome();
     print_menu();
 
     loop {
         let mut input: Option<i32> = None;
-        match current_state {
-            OwnState::Idle => {
-                input = read_raw_input();
+        match context.current_state {
+            fsm::OwnState::Idle => {
+                input = util::read_raw_input();
             },
             _ => ()
         };
-        state_logic(&mut current_state, input);
-        output_logic(&current_state, &mut todo_entries);
+
+        context.user_input = input;
+
+        fsm::state_logic(&mut context);
+        fsm::output_logic(&mut context);
     }
 
 }
